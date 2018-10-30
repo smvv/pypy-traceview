ba64f4633ffa44ce9545a094dec9d57d7ae9b507
Sander Mathijs van Veen <smvv@kompiler.org>
2018-10-30 10:36:39 +0100

    Add basic profiling support (use --profile flag)

--- pypy_traceview/main.py
+++ pypy_traceview/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import io
+import cProfile
+import pstats
 
 from pypy_traceview.logparser.pypylog import parse
 from pypy_traceview.snippet import resolve_code_snippets
@@ -15,20 +18,42 @@ parser.add_argument('file', metavar='FILE', type=str,
 parser.add_argument('--output', '-o', type=str, default='output.html',
                     help='HTML output filename (default: output.html)')
 
+parser.add_argument('--profile', action='store_true',
+                    help='Dump CPU profiler info')
+
 
 def main():
     args = parser.parse_args()
+
+    if args.profile:
+        pr = cProfile.Profile()
+        pr.enable()
+
     print('Opening PyPy log file: {}'.format(args.file))
 
     with open(args.file) as f:
         # TODO
         search_paths = ['testcases/simple-add-mod-loop']
+
+        print('Parsing PyPy log file')
         traces = parse(f)
+
+        print('Resolving code snippets')
         resolve_code_snippets(traces, search_paths)
+
+        print('Rendering HTML output')
         html = render(traces)
 
     with open(args.output, 'w') as f:
+        print('Writing to HTML output file: {}'.format(args.output))
         f.write(html)
+
+    if args.profile:
+        pr.disable()
+        stream = io.StringIO()
+        ps = pstats.Stats(pr, stream=stream).strip_dirs().sort_stats('cumtime')
+        ps.print_stats(20)
+        print(stream.getvalue())
 
 
 if __name__ == '__main__':
